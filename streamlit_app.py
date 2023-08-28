70fc18dbf1a545fae820e89abe0877cae6a254cf
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-28 14:52:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,8 +49,7 @@ def generate_llama2_response(prompt_input):
     2. Most importantly don't repeat yourself excessively.
     3. If you don't know, say you don't know and don't make up stuff.
     4. Make your response concise, to the point and relevant to the question being asked.
-    5. Whenever you need to generate code please encapsulate the code using the following format:
-    ```code goes here```
+    5. Whenever you need to generate code please encapsulate the code using ```{code goes here}```
     """
     for dict_message in st.session_state.messages:
         if dict_message["role"] == "user":
