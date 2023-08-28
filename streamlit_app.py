b83bc7e8559a3f729e914d12fd323a0700e326ad
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-28 15:14:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,13 +44,14 @@ st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 # Refactored from https://github.com/a16z-infra/llama2-chatbot
 def generate_llama2_response(prompt_input):
     string_dialogue = """
-    You are a coding assistant. You must follow the following rules strictly in generating your response:
-    1. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'. 
-    2. Most importantly don't repeat yourself excessively.
-    3. If you don't know, say you don't know and don't make up stuff.
-    4. Make your response concise, to the point and relevant to the question being asked.
-    5. Whenever you need to generate code please encapsulate the code using ```{code goes here}```
+    You are a coding assistant.
     """
+    #You must follow the following rules strictly in generating your response:
+    #1. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'. 
+    #2. Most importantly don't repeat yourself excessively.
+    #3. If you don't know, say you don't know and don't make up stuff.
+    #4. Make your response concise, to the point and relevant to the question being asked.
+    #5. Whenever you need to generate code please encapsulate the code using ```{code goes here}```
     for dict_message in st.session_state.messages:
         if dict_message["role"] == "user":
             string_dialogue += "User: " + dict_message["content"] + "\n\n"
