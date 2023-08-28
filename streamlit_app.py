885a87066224f8529febe84f73fc0af1d28e5993
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-28 13:58:32 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import replicate
 import os
 
 # App title
-st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
+st.set_page_config(page_title="🦙💬 Code Llama Chatbot")
 
 # Replicate Credentials
 with st.sidebar:
