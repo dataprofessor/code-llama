232022d04aafd2abfa82858f02760758f719d9dc
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-29 00:10:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,31 +43,26 @@ st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 # Function for generating LLaMA2 response
 # Refactored from https://github.com/a16z-infra/llama2-chatbot
 def generate_llama2_response(prompt_input):
+    
     string_dialogue = """
     You are a coding assistant.
+
+    You must follow the following rules strictly in generating your response:
+    1. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'. 
+    2. Most importantly don't repeat yourself excessively.
+    3. If you don't know, say you don't know and don't make up stuff.
+    4. Make your response concise, to the point and relevant to the question being asked.
+    5. Whenever you need to generate code please encapsulate the code using ```{code goes here}```
     """
-    #You must follow the following rules strictly in generating your response:
-    #1. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'. 
-    #2. Most importantly don't repeat yourself excessively.
-    #3. If you don't know, say you don't know and don't make up stuff.
-    #4. Make your response concise, to the point and relevant to the question being asked.
-    #5. Whenever you need to generate code please encapsulate the code using ```{code goes here}```
+    
     for dict_message in st.session_state.messages:
         if dict_message["role"] == "user":
             string_dialogue += "User: " + dict_message["content"] + "\n\n"
         else:
             string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
     #output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
-    prompt = """
-    Write code to solve the following coding problem and wrap your code answer, particularly using ``` and ``` before and after the code answer, respectively:
-
-    {prompt_input}
-
-    Assistant:
-    """
     output = replicate.run('replicate/codellama-7b-python:0135c7cd0ffb4917e53580562c8d528a46b64102546c27333e8ff8298a85798f',
-                           #input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
-                           input={"prompt": prompt,
+                           input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
                                   "temperature":temperature, "top_p":top_p, "top_k":top_k, "max_length":max_length, "repetition_penalty":1.15})
     return output
 
