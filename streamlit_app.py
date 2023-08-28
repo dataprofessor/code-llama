c3a40b361c3ad0e12f93b1f361e1c16d94d9fa9e
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-28 17:57:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,8 +59,11 @@ def generate_llama2_response(prompt_input):
             string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
     #output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
     prompt = """
+    User:
     Write code to solve the following coding problem that obeys the constraints of wrapping your code answer using ```:
     {prompt_input}
+    
+    Assistant:
     """
     output = replicate.run('replicate/codellama-13b:1c914d844307b0588599b8393480a3ba917b660c7e9dfae681542b5325f228db',
                            #input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
