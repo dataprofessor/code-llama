096151bf4cb6be0438b81cd7e54cb95d6a50463f
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-28 14:15:19 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="🦙💬 Code Llama Chatbot")
 
 # Replicate Credentials
 with st.sidebar:
-    st.title('🦙💬 Llama 2 Chatbot')
+    st.title('🦙💬 Code Llama Chatbot')
     if 'REPLICATE_API_TOKEN' in st.secrets:
         st.success('API key already provided!', icon='✅')
         replicate_api = st.secrets['REPLICATE_API_TOKEN']
